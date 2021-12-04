6c9e03e6f2abda5311c888e9070977cbf645ac30
p-sthely <contact@philippesthely.com>
2021-12-04 13:22:45 +0100

    always display social icons in a row flex layout

--- src/components/navbar.mjs
+++ src/components/navbar.mjs
@@ -3,7 +3,7 @@ const data = {
 }
 
 const template = `
-<nav class="navbar navbar-expand-lg navbar-dark bg-transparent py-4 px-2">
+<nav class="navbar navbar-expand-lg navbar-dark bg-transparent py-4 px-lg-2">
   <div class="container-fluid">
     <a class="navbar-brand fs-3" href="#">Paloma</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -12,27 +12,27 @@ const template = `
     <div class="collapse navbar-collapse flex-grow-0" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link" href="#">À propos</a>
+          <a class="nav-link text-light" href="#">À propos</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Épisodes</a>
+          <a class="nav-link text-light" href="#">Épisodes</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Blog</a>
+          <a class="nav-link text-light" href="#">Blog</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Contact</a>
+          <a class="nav-link text-light" href="#">Contact</a>
         </li>
       </ul>
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0 px-lg-4">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0 px-lg-4 flex-row">
         <li class="nav-item">
-          <a class="nav-link" href="#"><i class="bi bi-instagram"></i></a>
+          <a class="nav-link text-light px-2" href="#"><i class="bi bi-instagram"></i></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#"><i class="bi bi-youtube"></i></a>
+          <a class="nav-link text-light px-2" href="#"><i class="bi bi-youtube"></i></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#"><i class="bi bi-facebook"></i></a>
+          <a class="nav-link text-light px-2" href="#"><i class="bi bi-facebook"></i></a>
         </li>
       </ul>
       <a class="btn btn-secondary btn-lg rounded-0 px-4" role="button">Donner</a>
