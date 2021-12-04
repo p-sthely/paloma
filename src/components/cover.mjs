91de3bfd960a9f153cc2deb52d48afef7ab7fd2b
p-sthely <contact@philippesthely.com>
2021-12-04 16:19:32 +0100

    add image filter

--- src/components/cover.mjs
+++ src/components/cover.mjs
@@ -1,10 +1,19 @@
 const data = {
   style: {
-    img: `
+    imgFilter: `
       position: absolute;
       top: 0;
       left: 0;
       z-index: -1;
+      width: 100vw;
+      height: 100vh;
+      background-color: rgba(0,0,0,.5);
+    `,
+    img: `
+      position: absolute;
+      top: 0;
+      left: 0;
+      z-index: -2;
       width: 100vw;
       height: 100vh;
       object-fit: cover;
@@ -13,6 +22,7 @@ const data = {
 }
 
 const template = `
+<div style="${data.style.imgFilter}"></div>
 <img src="https://c.pxhere.com/photos/1d/9c/black_and_white_guitar_music_musician_sound-1068782.jpg!d" style="${data.style.img}" alt="a musician playing a yamaha guitar">
 `
 
